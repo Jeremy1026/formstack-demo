abeaa5cf5dd053022222c7961620f1fe213e7ed0
Jeremy Curcio <j.curcio@me.com>
2017-06-28 10:09:17 -0400

    Create new users

--- views/js/main.js
+++ views/js/main.js
@@ -1,21 +1,65 @@
-var foo = "bar";
-
 $(document).ready(function() {
 	console.log("Ready");
+updateUserList();
 });
 
 function deleteUser(id) {
-	console.log("Deleting user: "+id);
+	// console.log("Deleting user: "+id);
 	var request = $.ajax({
 	    url: '/api/deleteUser',
-	    type: 'DELETE',
-	    data: { id: id }
-	});
-	request.success(function(result) {
-	        // Do something with the result
-	        console.log(result);
-	    });
-	request.done(function(result) {
-	    	console.log(result);
-	    });
+	    type: 'POST',
+	    data: { 'id': id }
+	})
+	.done(function(result) {
+	   	console.log(result);
+    });
+}
+
+function createUser() {
+	$('#createUserForm').show("fast");
+}
+
+$("#createUserForm").submit(function(e) {
+	var request = $.ajax({
+	    url: '/api/createUser',
+	    type: 'POST',
+	    data: $("#createUserForm").serialize()
+    })
+	.done(function(result) {
+	   	console.log(result);
+	   	var parsedResult = JSON.parse(result);
+	   	if (parsedResult['success'] == true) {
+	   		updateUserList();
+	   		$("#createUserForm").find("input[type=text], input[type=password]").val("");
+   			$('#createUserForm').hide("fast");
+
+	   	}
+    });
+	e.preventDefault();
+})
+
+function updateUserList() {
+	var request = $.ajax({
+	    url: '/api/getUsers',
+	})
+	.done(function(result) {
+	   	var parsedResult = JSON.parse(result);
+	   	var list = "<tr><td>User ID</td>"+
+	   					 "<td>Email</td>"+
+	   					 "<td>First Name</td>"+
+	   					 "<td>Last Name</td>"+
+	   					 "<td>Password</td>"+
+	   					 "<td></td></tr>";
+	   	parsedResult.forEach(function(element) {
+	   		console.log(element);
+	   		list += "<tr>"+
+	   					"<td>"+element['id']+"</td>"+
+	   					"<td>"+element['first_name']+"</td>"+
+	   					"<td>"+element['last_name']+"</td>"+
+	   					"<td>"+element['password']+"</td>"+
+	   					"<td><a href='./user/edit'>Edit</a>/<a href='#' onclick='deleteUser("+element['id']+")''>Delete</a></td>"+
+	   				"</tr>";
+	   	});
+	   	$('.main').html(list);
+    });
 }
